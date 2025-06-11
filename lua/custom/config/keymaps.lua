fb4a51338a85cf87ba8c73768a5527ad7d966a95
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-06-11 18:21:16 +0900

    Revert "weird Ex not an editor command error."
    This reverts commit 3c2303ae78a2d02833f766bdf171e2f7dff14968.

--- lua/custom/config/keymaps.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-
--- vim.api.nvim_set_keymap('n', '<leader>e', ':Ex<CR>', { noremap = true, desc = 'Open file explorer' })
