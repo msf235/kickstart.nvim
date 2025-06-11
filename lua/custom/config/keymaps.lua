3c2303ae78a2d02833f766bdf171e2f7dff14968
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-06-11 15:39:59 +0900

    weird Ex not an editor command error.

---
+++ lua/custom/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+-- vim.api.nvim_set_keymap('n', '<leader>e', ':Ex<CR>', { noremap = true, desc = 'Open file explorer' })
