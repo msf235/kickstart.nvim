7cd95e313c3ba0c076cfdc7d996cdbabaf034b5c
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-06-25 13:58:14 +0900

    added back snippet folder load

--- init.lua
+++ init.lua
@@ -428,5 +428,7 @@ vim.api.nvim_create_user_command('GoToError', go_to_error, {})
 
 vim.keymap.set('n', 'gb', ':GoToBreakpoint<CR>', { silent = true, desc = 'Go To Breakpoint' })
 
+require('luasnip.loaders.from_vscode').lazy_load { paths = { './snippets' } }
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
