cf2ad6fff21cfceb9fa0a7a97001f6fa749f2ede
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-09-26 05:38:50 -0600

    added keymap for copy selected to system clipboard

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -1,4 +1,5 @@
 vim.keymap.set('n', 'Y', '"+yy', { desc = 'Copy line to system clipboard' })
+vim.keymap.set('v', 'Y', '"+y', { desc = 'Copy selected to system clipboard' })
 
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
