f2b0cf07d05fcd055d190189b32bd670de4ff6e0
Matthew Farrell <matthew.farrell.235@gmail.com>
2026-04-13 19:14:29 +0900

    sync

--- lua/custom/plugins/code-tools.lua
+++ lua/custom/plugins/code-tools.lua
@@ -415,7 +415,8 @@ return {
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
       -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_method = 'skim'
+      vim.g.vimtex_compiler_latexmk = { out_dir = 'build' }
     end,
   },
 }
