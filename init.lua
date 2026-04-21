2280a62a36f4393760319f73a2d10925125adc14
Matthew Farrell <matthew.farrell.235@gmail.com>
2026-04-21 13:03:58 +0900

    sync

--- init.lua
+++ init.lua
@@ -227,14 +227,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- vim.api.nvim_echo({ { 'INIT.LUA: early echo test', 'WarningMsg' } }, true, {})
-
--- pcall(require, 'trace_clangd')
-
--- pcall(require, 'trace_require_lspconfig.lua')
-
--- pcall(require, 'trace_vim_lsp_enable')
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
